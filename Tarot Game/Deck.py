dac3fe49ce192ca5cc71ca8b191760fb89340bd0
Luan <luan.mota@docnix.com.br>
2024-07-01 11:26:35 -0300

    Adicionado a possibilidade de carta de cabeca para baixo

--- Tarot Game/Deck.py
+++ Tarot Game/Deck.py
@@ -7,9 +7,8 @@ def get_card(deck):
     num = random.randint(0, len(deck) - 1)  # the minus 1 fixes the zero indexed array out-of-range error
     card = deck[num]
     del (deck[num])  # so we don't get the same card twice if we're calling this multiple times for the same hand
-    # rev = random.randint(-1, 1)  # is card reversed? zero (false) or 1 (true)
-    drawn = (card)  # tuple of card dictionary + 1 or zero for reversal
-    #print (int(drawn["rev"]))
+    rev = random.randint(-1, 1)  # is card reversed? zero (false) or 1 (true)
+    drawn = (card, rev)  # tuple of card dictionary + 1 or zero for reversal
     return drawn
 
 
--- Tarot Game/game.py
+++ Tarot Game/game.py
@@ -1,70 +1,88 @@
-from mimetypes import init
-import random
 import tkinter as tk
 from PIL import ImageTk, Image
 import Deck
-import string
 
 window = tk.Tk()
 window.title('Tarot')
-window.geometry("700x700")
-window.resizable(True, True)
+window.geometry("700x600")
+window.resizable(False, False)
+window.configure(background='#008080')
 
 deck = Deck.get_deck()
 Deck.deck_suffle(deck)
 
-
-label = tk.Label(window, text="Puxe uma Sorte!", font=('Calibri 15 bold'))
+label = tk.Label(window, text="Escolha a quantidade de Cartas!", font=('Calibri 15 bold'))
 label.pack(pady=20)
+label.configure(background='#008080')
 
-rodape = tk.Label(window, text="Feito por Luan", font=("Calibri 12 bold"))
-rodape.pack(pady=20, side=tk.BOTTOM)
-
-descricao = tk.Label(window, text="", font=("Calibri 12"), wraplength= 300)
-descricao.pack(pady=20, side=tk.BOTTOM)
-
-
-print(set(descricao.configure().keys()) - set(descricao.configure().keys()))
-
+# rodape = tk.Label(window, text="Feito por Luan", font=("Calibri 12 bold"))
+# rodape.pack(pady=20, side=tk.BOTTOM)
 
 def on_click_btn2():
-    carta = Deck.get_card(deck)
+
+    label.destroy()
+
+    window.geometry('1124x650')
+
+    carta_component()
+
+    carta_component()
+
+    carta_component()
+
+    btn1.destroy()
+    btn2.destroy()
+
 def on_click_btn1():
+    label.destroy()
+    carta_component()
+
+    btn2.destroy()
+    btn1.destroy()
+
+def carta_component():
     carta = Deck.get_card(deck)
+    informacao = carta[0]
+    reverso = carta[1]
+    print(reverso)
 
-    print(carta)
+    titulo = tk.Label(window, font=('Calibri 15 bold'))
+    titulo.pack(pady=20)
+    titulo.configure(background='#008080')
 
-    # carta['name']
-    # carta['image']
-    # carta['desc']
-    # carta['rdesc']
-    # carta['sequence']
-    # carta['cardtype']
-    # carta['message']
-
-    label["text"] = (carta['name'] )
-                     # + " - " + str(carta['sequence']))
-    image = Image.open(carta['image'])
+    titulo["text"] = (informacao['name'] + " - " + str(informacao['sequence']))
+    image = Image.open(informacao['image'])
     image = image.resize((175, 340), Image.LANCZOS)
 
+    if (reverso == -1):
+        image.rotate(180)
+
     image = ImageTk.PhotoImage(image)
-    label1 = tk.Label(image=image)
-    label1.image = image
-    #label1.pack(side="top")
-    #label1.pack(side="bottom", fill="both")
-    label1.place(x=260, y=80)
+    imagem = tk.Label(image=image)
+    imagem.image = image
+    imagem.place(x=260, y=80)
 
-    descricao["text"] = carta["desc"]
+    descricao = tk.Label(window, text="", font=("Calibri 12"), wraplength=450)
+    descricao.pack(pady=75, side=tk.BOTTOM)
+    descricao.configure(background='#008080')
+
+    if (reverso == -1):
+        descricao["text"] = informacao["rdesc"]
+    else:
+        descricao["text"] = informacao["desc"]
 
 
-btn1 = tk.Button(window, text="1X Sorte", command=on_click_btn1, width=15, padx=30)
+
+btn1 = tk.Button(window, text="1-Carta", command=on_click_btn1, width=15, padx=30)
 btn1.pack(pady=25)
 btn1.pack(side=tk.BOTTOM)
-btn1.place(x=140, y=550)
+btn1.place(x=140, y=500)
+btn1.configure(background='grey')
 
-btn2 = tk.Button(window, text="3X Sorte", command=on_click_btn2, width=15, padx=30)
+btn2 = tk.Button(window, text="3-Carta", command=on_click_btn2, width=15, padx=30)
 btn2.pack(pady=25)
 btn2.pack(side=tk.BOTTOM)
-btn2.place(x=340, y=550)
+btn2.place(x=340, y=500)
+btn2.configure(background='grey')
 
 window.mainloop()
