73d7e6f9eee67fcf1742ddae29dac9f1fc5fb881
yuhohah <luandepaulamota@hotmail.com>
2025-09-24 19:16:11 -0300

    Arrumando erros no linux

--- Tarot Game/utils/resource_path.py
+++ Tarot Game/utils/resource_path.py
@@ -8,6 +8,7 @@ def resource_path(relative_path):
         base_path = sys._MEIPASS
     except AttributeError:
         # Quando executado como um script Python
-        base_path = os.path.abspath(".")
+        base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
     return os.path.join(base_path, relative_path)
