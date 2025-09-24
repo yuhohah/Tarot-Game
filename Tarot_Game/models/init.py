2229a4819330bf774ce4a699bc3c78bcfbadb4d9
yuhohah <luandepaulamota@hotmail.com>
2025-09-24 19:36:09 -0300

    Cleaning

--- Tarot_Game/models/init.py
+++ Tarot_Game/models/init.py
@@ -1,5 +1,4 @@
 # Permite que o diretório models seja tratado como um pacote Python
 from .deck_manager import DeckManager
-from .Deck import Deck
 
-__all__ = ['DeckManager', 'Deck']
+__all__ = ['DeckManager']
